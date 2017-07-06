1a014e9b3baa37d7f9631a9de5280b47858dda03
Josh Stuart <josh@etaskr.com>
2017-07-06 10:44:24 +1000

    Added kraken. Still a bit hacky, but will continue to abstract the api calls to a single service to allow adding new exchange apis simple.

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 import EosApiService from './lib/eos/eos-api-service';
 import EosApiDataToEosPeriodDtoMapper from './lib/eos/eos-api-object-to-eos-period-dto-mapper';
-import BitfinnexApiService from './lib/exchanges/bitfinnex-api-service';
-import BitfinnexApiDataToPriceDtoMapper from './lib/exchanges/bitfinnex-api-data-to-eos-period-dto-mapper';
+import BitfinexApiService from './lib/exchanges/bitfinex/bitfinex-api-service';
+import BitfinexApiDataToPriceDtoMapper from './lib/exchanges/bitfinex/bitfinex-api-data-to-price-dto-mapper';
+import KrakenApiService from './lib/exchanges/kraken/kraken-api-service';
+import KrakenApiDataToPriceDtoMapper from './lib/exchanges/kraken/kraken-api-data-to-price-dto-mapper';
 import EosEthArbitrageService from './lib/arbitrage/eos-eth-arbirage-service';
 
 const eosApiDataToEosPeriodDtoMapper = new EosApiDataToEosPeriodDtoMapper();
 const eosApiService = new EosApiService(eosApiDataToEosPeriodDtoMapper);
-const bitfinnexApiDataToPriceDtoMapper = new BitfinnexApiDataToPriceDtoMapper();
-const bitfinnexApiService = new BitfinnexApiService(bitfinnexApiDataToPriceDtoMapper);
-const eosEthArbitrageService = new EosEthArbitrageService(eosApiService, bitfinnexApiService);
+const bitfinexApiDataToPriceDtoMapper = new BitfinexApiDataToPriceDtoMapper();
+const bitfinexApiService = new BitfinexApiService(bitfinexApiDataToPriceDtoMapper);
+const krakenApiDataToPriceDtoMapper = new KrakenApiDataToPriceDtoMapper();
+const krakenApiService = new KrakenApiService(krakenApiDataToPriceDtoMapper);
+const eosEthArbitrageService = new EosEthArbitrageService(
+    eosApiService,
+    [
+        bitfinexApiService,
+        krakenApiService
+    ]
+);
+const POLLING_CADENCE = 60 * 1000;
+const ETH_AMOUNT_TO_SELL = 1;
 
 const run = async () => {
     const currentPeriod = await eosApiService.getCurrentPeriod();
     console.log(`
     The current EOS period:
-    Starting on: ${currentPeriod.getPeriodStartDate().format('LLLL')}
+    Started on: ${currentPeriod.getPeriodStartDate().format('LLLL')}
     Ending on: ${currentPeriod.getPeriodEndDate().format('LLLL')}
     EOS token allocation: ${currentPeriod.getTokenAllocation()}
-    ETH commitment: ${currentPeriod.getCommitedEthereum()}
+    Current ETH commitment: ${currentPeriod.getCommitedEthereum()}
     `);
 
-    const eosEthArbitrageDto = await eosEthArbitrageService.getCurrentArbitrageOpportinutiy();
-    console.log(`
-    For 1 ETH committed to EOS you will get ${eosEthArbitrageDto.getReturnOnInvestment()} ETH back at current price of ${eosEthArbitrageDto.getCurrentPrice()} EOSETH
-    `);
+    const eosEthArbitrageDtos = await eosEthArbitrageService.getCurrentArbitrageOpportinuties();
+    for (const eosEthArbitrageDto of eosEthArbitrageDtos) {
+        console.log(`
+        ${eosEthArbitrageDto.getExchangeName()}:
+        For 1 ETH committed to EOS you will get ${eosEthArbitrageDto.getReturnOnInvestment()} ETH back at current price of ${eosEthArbitrageDto.getCurrentPrice()} EOSETH
+        Sell ${ETH_AMOUNT_TO_SELL / eosEthArbitrageDto.getCurrentPrice()} EOS to recoup ${ETH_AMOUNT_TO_SELL} ETH
+        `);
+    }
 };
 
 run();
+setInterval(run, POLLING_CADENCE);
